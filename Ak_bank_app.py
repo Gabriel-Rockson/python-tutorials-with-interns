e0157dca737373d49423e632dfb733450f823b24
Ak-Euph <akphrimps@gmail.com>
2023-07-06 11:04:05 -0700

    Update Ak_bank_app.py
    The savings account class

--- Ak_bank_app.py
+++ Ak_bank_app.py
@@ -2,3 +2,10 @@ class Account():
     def __init__(self, account_number: str, balance: float) -> None:
         self.account_number = account_number
         self.balance = balance
+
+class SavingsAccount(Account):
+    def __init__(self, account_number: str, balance: float, interest_rate, minimum_balance) -> None:
+        super().__init__(account_number, balance, )
+        self.interest_rate = interest_rate 
+        self.minimum_balance = minimum_balance
+        
