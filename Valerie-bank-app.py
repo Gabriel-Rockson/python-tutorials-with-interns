e0345b8717bba881dce7b58f61e07e6c838a62fd
ValHammond <113612898+Vlrhmm@users.noreply.github.com>
2023-07-06 10:52:02 +0100

    question2

--- Valerie-bank-app.py
+++ Valerie-bank-app.py
@@ -1,5 +1,13 @@
 
 class Account:
     def __init__(self,*,account_number: str,balance: float) -> None:
-        self.account_number=account_number
-        self.balance=balance 
+       self.account_number=account_number
+       self.balance=balance 
+
+class SavingsAccount(Account):
+   def __init__(self,interest_rate: float, minimum_balance: float): 
+       self.interest_rate= interest_rate
+       self.minimum_balance = minimum_balance
+       account_number=1234567890
+       balance=  300000000 
+       super().__init__(account_number, balance) 
