b25bfe11ea1444d3ac79ea8cff9e82d7eb7cfcdb
Ak-Euph <akphrimps@gmail.com>
2023-07-06 11:09:38 -0700

    Update Ak_bank_app.py
    Checking account class

--- Ak_bank_app.py
+++ Ak_bank_app.py
@@ -4,8 +4,13 @@ class Account():
         self.balance = balance
 
 class SavingsAccount(Account):
-    def __init__(self, account_number: str, balance: float, interest_rate, minimum_balance) -> None:
+    def __init__(self, account_number: str, balance: float, interest_rate: float, minimum_balance: float) -> None:
         super().__init__(account_number, balance, )
         self.interest_rate = interest_rate 
         self.minimum_balance = minimum_balance
-        
+
+class CheckingAccount(Account):
+    def __init__(self, account_number: str, balance: float, transaction_limit: int, transaction_fee: float) -> None:
+        super().__init__(account_number, balance)
+        self.transaction_limit = transaction_limit
+        self.transaction_fee = transaction_fee
